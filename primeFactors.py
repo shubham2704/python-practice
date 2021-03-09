eba3acb5fb1ffc6d560e466e9769f7bfcc51adb7
shubham2704 <cool.shubham2704@gmail.com>
2021-03-09 23:49:11 +0530

    Prime factors

---
+++ primeFactors.py
@@ -0,0 +1,18 @@
+# Finding the Prime Factors of a number
+
+n = int(input('Enter the Number : '))
+print('Factors are :')
+
+i = 1
+while(i<=n):
+    k = 0
+    if(n%i==0):
+        j = 1
+        while(j<=1):
+            if(i%j==0):
+                k+=1
+            j+=1
+        
+        if(k==2):
+            print(i)
+    i+=1 
