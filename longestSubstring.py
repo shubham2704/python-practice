44ce27bd479f845f14d5a556fe1917697b2ec0f9
shubham2704 <cool.shubham2704@gmail.com>
2021-03-31 20:11:18 +0530

    codearena hackerearth

---
+++ longestSubstring.py
@@ -0,0 +1,27 @@
+s = input().split(' ')
+s1,s2 = s[0],s[1]
+vow = 'aeiou'
+
+def lcs(S,T):
+    m = len(S)
+    n = len(T)
+    counter = [[0]*(n+1) for x in range(m+1)]
+    longest = 0
+    lcs_set = set()
+    for i in range(m):
+        for j in range(n):
+            if S[i] == T[j]:
+                c = counter[i][j] + 1
+                counter[i+1][j+1] = c
+                if c > longest:
+                    lcs_set = set()
+                    longest = c
+                    lcs_set.add(S[i-c+1:i+1])
+                elif c == longest:
+                    lcs_set.add(S[i-c+1:i+1])
+
+    return lcs_set
+res = lcs(s1,s2)
+result = next(iter(res))
+r = ''.join(x for x in result if x not in vow)
+print(r)
